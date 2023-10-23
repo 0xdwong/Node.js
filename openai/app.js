97d187fea0b108b04c22b0196a89f4f2d091bcad
0xdwong <0xdwong@gmail.com>
2023-10-23 16:22:36 +0800

    chatgpt system角色

--- openai/app.js
+++ openai/app.js
@@ -6,18 +6,23 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
-
-const prompt = `As an English-Chinese translator, your task is to accurately translate text between the two languages.`;
-
-const text = `hello world`
-
-async function main() {
+async function translate(text) {
+    const prompt = `As an English-Chinese translator, your task is to accurately translate text between the two languages.`;
     const completion = await openai.chat.completions.create({
-        messages: [{ role: 'user', content: prompt + text }],
+
+        messages: [
+            { role: 'system', content: prompt },
+            { role: 'user', content: prompt + text }
+        ],
         model: 'gpt-3.5-turbo',
     });
 
     console.log(completion['choices'][0]['message']['content']);
+}
+
+
+async function main() {
+    await translate(`hello world`);
 }
 
 main();
