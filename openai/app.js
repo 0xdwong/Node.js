455e66858c7aad9b3c263aaac86873387785eccb
0xdwong <0xdwong@gmail.com>
2023-10-23 16:29:27 +0800

    chatgpt 流式响应

--- openai/app.js
+++ openai/app.js
@@ -20,9 +20,26 @@ async function translate(text) {
     console.log(completion['choices'][0]['message']['content']);
 }
 
+async function translateByStream() {
+    const prompt = `As an English-Chinese translator, your task is to accurately translate text between the two languages.`;
+    const text = `The API is non-deterministic by default.`;
+    const stream = await openai.chat.completions.create({
+        messages: [
+            { role: 'system', content: prompt },
+            { role: 'user', content: prompt + text }
+        ],
+        model: 'gpt-3.5-turbo',
+        stream: true,
+    });
+
+    for await (const part of stream) {
+        console.log(part.choices[0]?.delta?.content || '');
+    }
+}
 
 async function main() {
-    await translate(`hello world`);
+    // await translate(`hello world`);
+    // await translateByStream();
 }
 
 main();
